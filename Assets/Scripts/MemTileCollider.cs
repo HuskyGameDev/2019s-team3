4d6a009d4fdd222188ae329f431b9b4eb7aaf388
Michael Spoehr <spoehrmichael@gmail.com>
2019-02-02 12:49:43 -0500

    Added pressure plate functionality (movement and sound) to Evan's mini game

--- Assets/Scripts/MemTileCollider.cs
+++ Assets/Scripts/MemTileCollider.cs
@@ -6,9 +6,10 @@ public class MemTileCollider : MonoBehaviour {
 
     private MemoryGame master;
     private int slaveID;
+    private bool isColliding = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         master = GameObject.Find("MemoryGameBounds").GetComponent<MemoryGame>();
 	}
 	
@@ -16,15 +17,7 @@ public class MemTileCollider : MonoBehaviour {
 	void Update () {
 		
 	}
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            master.receiveInput(slaveID);
-        }
-    }
-
+    
     public void setID(int ID)
     {
         slaveID = ID;
@@ -33,5 +26,25 @@ public class MemTileCollider : MonoBehaviour {
     public void setMaterial(Material m)
     {
         GetComponent<MeshRenderer>().material = m;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (isColliding) return;
+            isColliding = true;
+
+            master.receiveInput(slaveID);
+            transform.Translate(0, -.1f, 0);
+            AkSoundEngine.PostEvent("Button", this.gameObject);
+        }
+    }
+       
+    private void OnTriggerExit(Collider other)
+    {
+        transform.Translate(0, .1f, 0);
+        isColliding = false;
+        AkSoundEngine.PostEvent("Button", this.gameObject);
     }
 }
