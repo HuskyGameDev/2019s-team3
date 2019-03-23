8824dfb853cd32a55e4440c453f11e5100e3e999
Christian Clemmons <ccclemmo@mtu.edu>
2019-03-23 14:46:35 -0400

    Trigger and start of 7th puzzle
    Added the dialog trigger to the maze and started the 7th puzzle

---
+++ Assets/Scripts/MazeAreaTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MazeAreaTrigger : MonoBehaviour {
+	private int dialogStep = -1;
+	private GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(dialogStep){
+		case 0:
+			if(!gameManager.IsShowingDialog()){
+				gameManager.ShowDialog("Welcome to Maze of Confusion, Reggie!");
+				dialogStep++;
+	}
+			break;
+		case 1:
+			if(!gameManager.IsShowingDialog()){
+				gameManager.ShowDialog("If you can get through this maze, you may find the key to success!");
+				dialogStep = -1;
+			}
+			break;
+	}
+}
+}
