ee312e8e1c0a00f991aa190bdeda7ac24754e2ee
Evan Overweg <32006764+Kavatika@users.noreply.github.com>
2019-03-23 15:41:17 -0400

    More basic work on monkey AI

--- Assets/Scripts/MonkeyThrowingAI.cs
+++ Assets/Scripts/MonkeyThrowingAI.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonkeyAI : MonoBehaviour {
+public class MonkeyThrowingAI : MonoBehaviour
+{
 
     Transform player;
     NavMeshAgent nav;
@@ -11,36 +12,36 @@ public class MonkeyAI : MonoBehaviour {
     bool isClose;
     public Throwable projectile;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         nav = GetComponent<NavMeshAgent>();
         thisAI = GetComponent<Transform>();
         projectile.PickUp(thisAI);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Vector3.Distance(thisAI.position, player.position) < 10)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(thisAI.position, player.position) < 10)
         {
-         
+
             shootPlayer();
         }
         else
         {
-            nav.enabled = true;
-            nav.SetDestination(player.position);
-            
+
         }
-	}
+    }
 
     void shootPlayer()
     {
         NavMeshHit hit;
-        if(nav.Raycast(player.position, out hit))
+        if (nav.Raycast(player.position, out hit))
         {
             Debug.Log("player is targetable");
-            
+
         }
 
     }
