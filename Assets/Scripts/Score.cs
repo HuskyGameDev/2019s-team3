379c8f3b04ab3f22c2ae110d688b7c2516b73a58
Christian Clemmons <ccclemmo@mtu.edu>
2019-04-06 14:33:13 -0400

    Ball Toss
    Implemented the hoops for the ball toss, made changes to the arena for ball toss, created a object respawn for the ball, created a score script to track the score, and implemented script for dialog.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    private bool gotKey = false;
+    int ringScore = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ringScore++;
+        if (ringScore == 3)
+        {
+            gotKey = true;
+        }
+    }
+
+    
+}
